c544089e883739c065dec24bd7bff42e3a0416ee
Christian Fetzer <fetzer.ch@googlemail.com>
2013-02-26 21:33:23 +0100

    [games] fix crash when PVR is used

--- xbmc/FileItem.cpp
+++ xbmc/FileItem.cpp
@@ -144,6 +144,7 @@ CFileItem::CFileItem(const CEpgInfoTag& tag)
   m_pvrRecordingInfoTag = NULL;
   m_pvrTimerInfoTag = NULL;
   m_pictureInfoTag = NULL;
+  m_gameInfoTag = NULL;
 
   Reset();
 
@@ -167,6 +168,7 @@ CFileItem::CFileItem(const CPVRChannel& channel)
   m_pvrRecordingInfoTag = NULL;
   m_pvrTimerInfoTag = NULL;
   m_pictureInfoTag = NULL;
+  m_gameInfoTag = NULL;
 
   Reset();
   CEpgInfoTag epgNow;
@@ -216,6 +218,7 @@ CFileItem::CFileItem(const CPVRRecording& record)
   m_pvrRecordingInfoTag = NULL;
   m_pvrTimerInfoTag = NULL;
   m_pictureInfoTag = NULL;
+  m_gameInfoTag = NULL;
 
   Reset();
 
@@ -235,6 +238,7 @@ CFileItem::CFileItem(const CPVRTimerInfoTag& timer)
   m_pvrRecordingInfoTag = NULL;
   m_pvrTimerInfoTag = NULL;
   m_pictureInfoTag = NULL;
+  m_gameInfoTag = NULL;
 
   Reset();
 
